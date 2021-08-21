890db7fe077d4a751fb726e17a0a81fdca767de6
yazantafesh <yazantafesh1@gmail.com>
2021-08-21 18:31:59 +0300

    sandbox test

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import Form from '../form/Form';
+import axios from 'axios';
 
 import List from '../list/List';
-import { v4 as uuid } from 'uuid';
+
 import "@blueprintjs/core/lib/css/blueprint.css";
+
+const todoAPI = 'https://api-js401.herokuapp.com/api/v1/todo';
+
+let config = {
+  headers: {
+    mode: 'cors',
+    cache: 'no-cache',
+    'Content-Type': 'application/json',
+  },
+};
 
 
 const ToDo = () => {
@@ -12,27 +23,71 @@ const ToDo = () => {
   const [incomplete, setIncomplete] = useState([]);
 
   function addItem(item) {
-    let data = { id: uuid(), text: item.text, assignee: item.assignee, complete: false, difficulty: item.difficulty }
-    setList([...list, data]);
-  }
 
-  // function deleteItem(id) {
-  //   const items = list.filter( item => item.id !== id );
-  //   setList(items);
-  // }
+    item.due = new Date();
+    fetch(todoAPI, {
+      method: 'post',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(item)
+    })
+      .then(response => response.json())
+      .then(savedItem => {
+        setList([...list, savedItem])
+      })
+      .catch(console.error);
+  }
 
   function toggleComplete(id) {
 
-    const items = list.map(item => {
-      if (item.id == id) {
-        item.complete = !item.complete;
-      }
-      return item;
-    });
+    let item = list.filter(i => i._id === id)[0] || {};
 
-    setList(items);
+    if (item._id) {
 
+      item.complete = !item.complete;
+
+      let url = `${todoAPI}/${id}`;
+
+      fetch(url, {
+        method: 'put',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(item)
+      })
+        .then(response => response.json())
+        .then(savedItem => {
+          setList(list.map(listItem => listItem._id === item._id ? savedItem : listItem));
+        })
+        .catch(console.error);
+    }
   }
+
+  const getTodoItems = () => {
+    fetch(todoAPI, {
+      method: 'get',
+      mode: 'cors',
+    })
+      .then(data => data.json())
+      .then(data => setList(data.results))
+      .catch(console.error);
+  };
+
+  const handleDelete = async (id) => {
+    let url = `${todoAPI}/${id}`;
+    const method = 'delete';
+
+			// if (item._id) {
+				const results = await axios[method](url, config);
+				setList(
+					list.filter(
+						(listItem) => listItem._id !== results.data._id,
+					),
+				);
+  }
+
+  useEffect(getTodoItems, []);
 
   useEffect(() => {
     let incompleteCount = list.filter(item => !item.complete).length;
@@ -44,7 +99,7 @@ const ToDo = () => {
     <>
       <h1>To Do List: {incomplete} Items Pending</h1>
       <Form addItem={addItem} />
-      <List list={list} toggleComplete={toggleComplete} />
+      <List list={list} toggleComplete={toggleComplete} handleDelete={handleDelete}/>
     </>
   );
 };
